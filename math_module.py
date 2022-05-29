60447f76071065f929dae6b175ae067b237e55e0
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-29 22:32:49 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,26 @@
+'''to check what are the function in math module'''
+# import math
+# print(dir(math))
+
+
+# print(4e1)#40.0 #no need to use math module for e
+# print(2e5)#200000.0
+# print(3e3)#40.0
+# print(1.2e4)#12000.0
+
+
+# import math as m
+#e=2.718281828459045
+# print(m.exp(3))#e*e*e=20.085536923187668
+# print(m.exp(2))#7.38905609893065=e*e
+
+#import math as m
+# print(m.sin(30))#-0.9880316240928618
+# print(m.cos(30))#0.15425144988758405
+# print(m.cosh(3))#10.067661995777765
+# from fractions import Fraction as f
+# print(f(2,5)) #2/5
+# print(f(10,2)) #5
+# print(f(2.5)) #5/2
+# print(f(5,0)) #zero division error
+
